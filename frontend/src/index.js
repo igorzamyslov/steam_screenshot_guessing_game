83b863fd79402b5902d7eec7692db05f4449cce8
Igor Zamyslov <igor.zamyslov@cognostics.de>
2021-10-22 11:14:02 +0200

    Update frontend implementation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SteamService from 'services/SteamService';
 import './index.css';
 import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root')
-
-ReactDOM.render(<p>Loading ...</p>, rootElement)
-
-SteamService.initAllApps().then(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  )
-})
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- frontend/src/services/SteamService.js
+++ frontend/src/services/SteamService.js
@@ -1,16 +1,8 @@
 import request from 'superagent';
 
 class SteamService {
-  async initAllApps () {
-    const response = await request.get('/api/apps');
-    if (Array.isArray(response.body)) {
-      this.apps = response.body;
-    } else {
-      this.apps = [];
-    }
-  }
-
-  getAppData = (appId) => request.get(`/api/app/${appId}`);
+  static getAppData = (appId) => request.get(`/api/app/${appId}`);
+  static getRandomAppData = () => request.get(`/api/app/random`);
 }
 
-export default new SteamService();
+export default SteamService;
