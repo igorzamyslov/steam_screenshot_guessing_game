a380288dff82c71b0023f8dfe33976a0ebf93994
Igor Zamyslov <zamyslovigor@gmail.com>
2021-10-24 13:51:44 +0200

    Use Steam API to gather info about a game

--- backend/database_handler/steam_handler.py
+++ backend/database_handler/steam_handler.py
@@ -4,50 +4,57 @@ from functools import cached_property
 from typing import Dict, List, Optional
 
 import requests
-from bs4 import BeautifulSoup
 
 
-def get_steam_apps() -> Dict[int, str]:
-    """ Acquires all Steam apps and returns them as dictionary {id: name} """
-    json_response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2").json()
-    return {steam_app["appid"]: steam_app["name"]
-            for steam_app in json_response["applist"]["apps"]}
+class SteamAppDataError(Exception):
+    """ Raised in case acquired app data wasn't successful """
+
+
+class SteamAppResponseError(Exception):
+    """ Raised in case response wasn't successful """
 
 
 @dataclass
-class SteamStorePageParser:
+class SteamAppHandler:
     app_id: int
+    
+    def __post_init__(self):
+        """ Init app data """
+        response = requests.get("https://store.steampowered.com/api/"
+                                f"appdetails?appids={self.app_id}")
+        try:
+            response.raise_for_status()
+        except Exception as error:
+            raise SteamAppResponseError from error
 
-    @cached_property
-    def app_store_page_soup(self) -> BeautifulSoup:
-        """ Get HTML of the app page """
-        page = requests.get(f"https://store.steampowered.com/app/{self.app_id}").text
-        return BeautifulSoup(page, "html.parser")
+        app_dict = response.json()[str(self.app_id)]
+        if not app_dict["success"]:
+            raise SteamAppDataError(f"Response for the app with ID {self.app_id} is not successful")
+        self.app_data = app_dict["data"]
+
+    @staticmethod
+    def get_steam_apps() -> Dict[int, str]:
+        """ Acquires all Steam apps and returns them as dictionary {id: name} """
+        json_response = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v2").json()
+        return {steam_app["appid"]: steam_app["name"]
+                for steam_app in json_response["applist"]["apps"]}
 
     def get_screenshot_urls(self) -> List[str]:
-        """ Parse app store page and return all screenshot URLs """
-        elements = self.app_store_page_soup.find_all(
-            "a", attrs={"class": "highlight_screenshot_link"})
-        return [url for element in elements
-                if (url := element.get("href")) is not None]
+        """ Parse app data and return all screenshot URLs """
+        return [s["path_full"] for s in self.app_data.get("screenshots", [])]
 
     def get_reviews_count(self) -> Optional[int]:
-        """ Parse app store page and return reviews count """
-        try:
-            # reviews div can contain multiple elements, we n
-            reviews_div = self.app_store_page_soup.find("div", string="All Reviews:")
-            count_div = (reviews_div.next_sibling.next_sibling
-                         .find("meta", attrs={"itemprop": "reviewCount"}))
-            return int(count_div.get("content"))
-        except (AttributeError, ValueError, TypeError):
-            return None
+        """ Parse app data and return reviews count """
+        if (recs := self.app_data.get("recommendations")) is not None:
+            return recs["total"]
+        return None
 
     def get_release_date(self) -> Optional[date]:
-        """ Parse app store page and return release date """
-        try:
-            date_div = (self.app_store_page_soup
-                        .find("div", attrs={"class": "release_date"})
-                        .find("div", attrs={"class": "date"}))
-            return datetime.strptime(date_div.string, "%d %b, %Y").date()
-        except (AttributeError, ValueError):
-            return None
+        """ Parse app data and return release date """
+        release_date_dict = self.app_data["release_date"]
+        if not release_date_dict["coming_soon"]:
+            try:
+                return datetime.strptime(release_date_dict["date"], "%d %b, %Y").date()
+            except ValueError:
+                return None
+        return None
