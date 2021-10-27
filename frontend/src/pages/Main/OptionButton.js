7f9ee9b00e12ef6ba76302b7314095c4ea540d18
alex <sealexey@gmail.com>
2021-10-28 00:25:43 +0200

    blick correct answer

--- frontend/src/pages/Main/OptionButton.js
+++ frontend/src/pages/Main/OptionButton.js
@@ -3,48 +3,50 @@ import Button from "react-bootstrap/Button";
 
 class OptionButton extends Component {
     constructor(props) {
-      super(props)
-      console.log("Props ",props)
-      this.state = {
-        btnClass : "base",      
-      }
-      this.blinkInterval = null
+        super(props)
+        console.log("Props ", props)
+        this.state = {
+            btnClass: "base",
+        }
+        this.blinkInterval = null
     }
-    
-    componentDidMount() {
-      this.updateBlink({}, this.props)
-    }
-  
-    componentDidUpdate(prevProps) {
-      this.updateBlink(prevProps, this.props)
-    }
-  
-    updateBlink = (prevProps, props) => {
-      const { blink: prevBlink } = prevProps;
-      const { blink, btnClass } = this.props;
-      console.log(btnClass,blink)
-      if (!prevBlink && blink) {
-        // create blinking interval
-        // this.blinkInterval = setInterval(() => {
-        //   this.setState({ btnClass: this.state.btnClass ? null : btnClass })
-        // }, 2000)
-      } else if (prevBlink && !blink) {
-        // remove blinking interval
-        clearInterval(this.blinkInterval)
-      }
-    }
-  
-    render() {
-      const { answer } = this.props;
-      const { btnClass } = this.state;
-      return (
-        <Button 
-          className={"option-btn " + btnClass}        
-        >
-          {answer.appName}
-        </Button>
-      )
-    }
-  }
 
-  export default OptionButton;
+    componentDidMount() {
+        this.updateBlink({}, this.props)
+    }
+
+    componentDidUpdate(prevProps) {
+        this.updateBlink(prevProps, this.props)
+    }
+
+    updateBlink = (prevProps, props) => {
+        const { blink: prevBlink } = prevProps;
+        const { blink, btnClass } = this.props;
+        console.log(btnClass, blink)
+        if (!prevBlink && blink) {
+            // create blinking interval
+            if (props.answer.correct) {
+                this.blinkInterval = setInterval(() => {
+                    this.setState({ btnClass: this.state.btnClass ? null : btnClass })
+                }, 300)
+            }
+        } else if (prevBlink && !blink) {
+            // remove blinking interval
+            clearInterval(this.blinkInterval)
+        }
+    }
+
+    render() {
+        const { answer } = this.props;
+        const { btnClass } = this.state;
+        return (
+            <Button
+                className={"option-btn " + btnClass}
+            >
+                {answer.appName}
+            </Button>
+        )
+    }
+}
+
+export default OptionButton;
