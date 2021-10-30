882bbaebf6ddcefd493f2b8a717510b2fd08ce3e
alex <sealexey@gmail.com>
2021-10-30 23:25:07 +0200

    add live heart component

---
+++ frontend/src/components/LiveHeart/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import { ReactComponent as FullHeart } from '../../assets/heart-fill.svg';
+
+const LiveHeart = (props) => {  
+  return (
+    <FullHeart className="live-heart"/>
+  );
+};
+
+export default LiveHeart;
