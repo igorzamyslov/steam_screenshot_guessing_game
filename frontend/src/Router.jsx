a5f64c908f8cc540da64e91be3fb313d60faa60f
alex <sealexey@gmail.com>
2021-11-10 11:48:20 +0100

    add menu component to Router.js

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -7,10 +7,11 @@ import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import ym from "react-yandex-metrika";
 
 export const routes = {
+  menuPage: "/menu",
+  mainPage: "/",
   resultPage: "/result",
   aboutPage: "/about",
   leaderboardPage: "/leaderboard",
-  mainPage: "/",
 };
 
 export function createNavigationHandler(history, route, queryParams = {}) {
@@ -32,8 +33,9 @@ export function createNavigationHandler(history, route, queryParams = {}) {
 export const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path={routes.resultPage} component={Result} />
+      <Route exact path={routes.menuPage} component={Menu} />
       <Route exact path={routes.mainPage} component={Main} />
+      <Route exact path={routes.resultPage} component={Result} />
       <Route exact path={routes.aboutPage} component={About} />
       <Route exact path={routes.leaderboardPage} component={Leaderboard} />
       <Redirect to={routes.mainPage} />
