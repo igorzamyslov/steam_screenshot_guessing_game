7ad22e616b3ce8a893ddebefcef10bb5e5e7553e
alex <sealexey@gmail.com>
2022-08-08 22:26:46 +0200

    add wrong and correct answers

--- frontend/src/pages/GeneratedQuiz/view.jsx
+++ frontend/src/pages/GeneratedQuiz/view.jsx
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 
 import {createNavigationHandler, routes} from "@/Router";
 import SSGGService from "@/services/SSGGService";
@@ -12,7 +12,10 @@ class GeneratedQuiz extends Component {
         this.state = {
             max: data.length,
             currentUrl: "",
-            description: ""
+            description: "",
+            inputText: "",
+            correct: "",
+            wrong: ""
         };
 
         this.navigateToMenu = createNavigationHandler(
@@ -20,12 +23,40 @@ class GeneratedQuiz extends Component {
             routes.menuPage
         );
 
+
         this.randomiseImage = () => {
             var imgIndex = Math.floor(Math.random() * this.state.max);
-            this.setState({description: data[imgIndex].description, currentUrl: data[imgIndex].url})
+            this.setState({
+                description: data[imgIndex].description.toLowerCase(),
+                currentUrl: data[imgIndex].url,
+                correct: "",
+                inputText: "",
+                wrong: "",
+            })
         }
     }
 
+    _onChange = (event) => {
+        this.setState({inputText: event.target.value});
+    }
+
+    _onKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            let words = this.state.description.split(" ");
+            let input = this.state.inputText.toLowerCase();
+
+            if (words.includes(input)) {
+                let old = this.state.correct
+                this.setState({correct: old + " " + input})
+                this.setState({inputText: ""})
+            } else {
+                let old = this.state.wrong
+                this.setState({wrong: old + " " + input})
+                this.setState({inputText: ""})
+            }
+
+        }
+    }
 
     componentDidMount() {
         this.randomiseImage();
@@ -44,6 +75,18 @@ class GeneratedQuiz extends Component {
                              key="quiz-image"
                         />
                     </div>
+                    <div> Words: {this.state.description.split(" ").length} </div>
+                    <input
+                        className="text-input"
+                        type="text"
+                        id="message"
+                        name="message"
+                        value={this.state.inputText}
+                        onChange={this._onChange}
+                        onKeyDown={this._onKeyDown}
+                    />
+                    <div> Correct: {this.state.correct}</div>
+                    <div> Wrong:{this.state.wrong}</div>
                     <div className="description-test">{this.state.description}</div>
                 </div>
             </MainTemplate>
